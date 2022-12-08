35b18bcf64051aa81bddb92a3042538be6bbc247
yajimoni419 <yagbosaheed52@gmail.com>
2022-12-08 08:01:00 -0800

    1

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -7,9 +7,11 @@
  * Return Always 0 (success)
  */
 
-int main(void){
-	printf("\"with proper grammar, but the outcome is a piece of art,");
-	return(0);
+int main(void) {
+
+
+	printf ("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
 
 
 }
