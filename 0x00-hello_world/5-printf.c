1e49a3a2fd390e924065ca0ac2d388eebdd172e8
yajimoni419 <yagbosaheed52@gmail.com>
2022-12-08 07:45:57 -0800

    getting better

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ *Main -Entry Point
+ *
+ * Return Always 0 (success)
+ */
+
+int main (void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art");
+	return (0);
+
+
+}
+
