5afaf5cd628f8939dca57d42bc7e9f37644e3154
yajimoni419 <yagbosaheed52@gmail.com>
2022-12-16 01:38:41 -0800

    debugging

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * largest_number - returns to the largest number
+ * @a: first integer
+ * @b: second number
+ * @c: third number
+ * Return: laster number
+ */
+
+int largest_number(int a, int b, int c);
+
+{
+
+	int largest;
+
+
+	if ((a > b && b > c));
+
+	else
+
+	{
+	largest = a;
+	}
+	else
+
+	{
+
+	largest = b;
+	}
+	else
+	{
+	largest = c;
+	}
+	return (largest);
+	}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+void postitive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int day, int month, int year);
+
+#endif
