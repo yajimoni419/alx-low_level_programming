214d99bc3068c198456db64e99901226729ae61c
YAGBOYAJU MUHAMMAD <118960622+yajimoni419@users.noreply.github.com>
2022-12-20 13:12:02 +0100

    Update 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
-
 /**
  * _puts - prints a string
  * @str: string to be printed
  */
-void _puts(char *str) /* This is my declearation */
+void _puts(char *str) /* _putchar is to be used */
 {
 	while (*str != '\0')
 	{
