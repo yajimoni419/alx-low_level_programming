b79de0b54f699d8b5448d594197d6370927b5075
YAGBOYAJU MUHAMMAD <118960622+yajimoni419@users.noreply.github.com>
2023-03-16 14:29:50 +0100

    wii

---
+++ 0x00-hello_world/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    return (0);
+}
+julien@ubuntu:~/c/0x00$ export CFILE=main.c
+julien@ubuntu:~/c/0x00$ ./0-preprocessor 
+julien@ubuntu:~/c/0x00$ tail c
