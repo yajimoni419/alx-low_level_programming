ded47fb29bcb01b893268a37bcb72053e82cabee
yajimoni419 <yagbosaheed52@gmail.com>
2023-03-18 13:31:29 -0700

    fail to fsil

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,18 +9,21 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
+
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-        if (n > 0)
-	printf("%d is postive\n", n);
+
+	if (n > 0)
+	printf("%d is positive\n", n);
 	else if (n == 0)
 	printf("%d is zero\n", n);
-	else 
+	else
 	printf("%d is negative\n", n);
 	return (0);
 }
