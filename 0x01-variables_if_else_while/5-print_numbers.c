ea9cce0812c3e6290bb737697f78f4496ced33ab
yajimoni419 <yagbosaheed52@gmail.com>
2023-03-19 07:34:38 -0700

    king

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the heart of my code
+ *
+ * Return: always (0) success
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d", i);
+
+	printf("\n");
+
+	return (0);
+}
