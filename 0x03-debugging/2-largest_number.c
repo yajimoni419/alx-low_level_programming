82ff995673bb70a59cb1c83b9a7b8a793c7521a4
yajimoni419 <yagbosaheed52@gmail.com>
2023-03-22 10:23:42 -0700

    pluto mania

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -8,29 +8,27 @@
  * Return: laster number
  */
 
-int largest_number(int a, int b, int c);
+int largest_number(int a, int b, int c)
 
 {
 
 	int largest;
 
-
-	if
-((a > b && b > c));
-
-	else
-
+	if (a > b && b > c)
 	{
-
 	largest = a;
 	}
-	else
-
+	else if (a > c && c > b)
+	largest = a;
+	else if (b > a && a > c)
 	{
-
-	largest = b;
+		largest = b;
 	}
-	else
+	else if (b > c && c > a)
+		largest = b;
+	else if (c > a && a > b)
+		largest = c;
+	else if (c > b && b > a)
 	{
 	largest = c;
 	}
