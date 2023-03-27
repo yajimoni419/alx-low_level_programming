496bfbfc4c3887f9775e1d8a9c69d0e11f878285
yajimoni419 <yagbosaheed52@gmail.com>
2023-03-27 05:49:32 -0700

    fasting

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,12 @@
 #include "main.h"
 
 /**
- * reset_to_98_ - update the value of n to 98
+ * reset_to_98 - update the value of n to 98
  * @n: input
  * Return: n
  */
-void return_to_98(int *n)
+
+void reset_to_98(int *n)
 {
 	*n = 98;
-}	
+}
