c93d0af9d38bf0afd4391367c53f918f0a1f45fc
yajimoni419 <yagbosaheed52@gmail.com>
2023-03-28 13:10:02 -0700

    smd baba

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,7 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include "_putchar.c"
 /**
  * print_rev - prints reversed string, followed by a new line
  * @s: pointer to the string to print
+ * _putchar - print the character for substitute
  * Return: void
 */
 
@@ -15,6 +18,6 @@ while (i--)
 {
 	_putchar(s[i]);
 }
-_putchar('\n');
+	_putchar('\n');
 
 }
